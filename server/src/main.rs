b6c23995df6e30946e21955d2f28c5b6e5eb58e3
Athul Anoop <athulanoop2020@gmail.com>
2025-03-26 23:37:55 +0530

    update: partial server code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod middlewares;
+mod routes;
+mod state;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // setup database connection
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("Database URL required: DATABASE_URL environment variable must be set");
+    let pg_pool = sqlx::postgres::PgPoolOptions::new()
+        .connect(&database_url)
+        .await?;
+    println!("{:?}", pg_pool);
+    let api = axum::Router::new()
+        .route_layer(axum::middleware::from_fn_with_state(
+            state.clone(),
+            middlewares::auth_middleware::authenticate,
+        ))
+        .nest("/auth", routes::auth_routes::auth_router());
+    Ok(())
 }
